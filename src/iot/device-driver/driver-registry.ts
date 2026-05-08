272052c13cab388feff9f48328b6fb20200cf253
Cavallaro Catello Stefano <stefano@stepo.biz>
2026-05-08 13:15:16 +0200

    Sonoff driver

--- src/iot/device-driver/driver-registry.ts
+++ src/iot/device-driver/driver-registry.ts
@@ -1,11 +1,14 @@
 import { IotProtocolDriver } from './iot-protocol-driver';
 import { ShellyHttpDriver } from './shelly-http.driver';
 import { ShellyMqttDriver } from './shelly-mqtt.driver';
+import { SonoffDiyDriver } from './sonoff-diy.driver';
 
 const shellyHttp = new ShellyHttpDriver();
 const shellyMqtt = new ShellyMqttDriver();
+const sonoffDiy = new SonoffDiyDriver();
 
 export const driverRegistry: Record<string, IotProtocolDriver> = {
   [shellyHttp.protocol]: shellyHttp,
   [shellyMqtt.protocol]: shellyMqtt,
+  [sonoffDiy.protocol]: sonoffDiy,
 };
